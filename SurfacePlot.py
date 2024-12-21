784e0aab21f752edb23b214e3a23f357e175c9b9
Aman Verma <eez227536@ee.iitd.ac.in>
2024-12-21 21:05:59 +0530

    SurfacePlot optimized!

--- SurfacePlot.py
+++ SurfacePlot.py
@@ -1,12 +1,6 @@
 ####### Importing Libraries
-import os
-import random
 import numpy as np
-import tensorflow as tf
 import matplotlib.pyplot as plt
-from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D 
-from matplotlib.ticker import LinearLocator
 from hyperparameter import compute_acceptance
 
 ####### Surface Plot
